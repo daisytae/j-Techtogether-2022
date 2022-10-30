c7620ac029570bec7be7de9bcc4a623fb4e67dfc
daisytae <116963811+daisytae@users.noreply.github.com>
2022-10-30 11:22:11 -0400

    Add files via upload
    10/30-11:23

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var  hydrationImage;
 var plasticImage; 
+var whyImage;
+var homeImage;
 
 const hydrationList = document.getElementsByClassName("hydration");
 hydrationImage = hydrationList[0];
 hydrationImage.addEventListener("click", myFunction);
 
 function myFunction() {
+    location.href = "file:///C:/Users/mylit/OneDrive/Documents/GitHub/j-Techtogether-2022/hydration.html";
     console.log("Hi");
  
 }
@@ -15,8 +18,17 @@ plasticImage = plasticList[0];
 plasticImage.addEventListener("click", wyFunction);
 
 function wyFunction() { 
+    location.href = "file:///C:/Users/mylit/OneDrive/Documents/GitHub/j-Techtogether-2022/plastic.html";
     console.log("bye");
 }
 
+const homeList = document.getElementsByClassName("home");
+homeImage = homeList[0];
+homeImage.addEventListener("click", ymFunction);
+
+function ymFunction() {
+    location.href = "file:///C:/Users/mylit/OneDrive/Documents/GitHub/j-Techtogether-2022/index.html";
+    console.log("hm");
+}
 
 
